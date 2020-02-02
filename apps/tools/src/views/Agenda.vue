ed4462e0fd0df901ad4b0091c43ec79766bf938c
Ruud van der Weide <rj@rdcl.dev>
2020-02-02 14:15:24 +0100

    Made some generalizations in the Concert Agenda, to make it easier to expand upon in the future.

--- apps/tools/src/views/Agenda.vue
+++ apps/tools/src/views/Agenda.vue
@@ -2,6 +2,13 @@
   <div>
     <h1>Concert Agenda</h1>
 
+    <p v-if="loading > 0">Een moment geduld, ik ben alles aan het ophalen ⏳.</p>
+
+    <p v-if="errors.length > 0">Hm... Het lijkt er op dat er iets mis is gegaan 🤔.</p>
+    <ul v-if="errors.length > 0">
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+
     <section
         :class="`event ${ event.expanded ? 'event--expanded' : 'event--collapsed' }`"
         v-for="event in events"
@@ -37,7 +44,7 @@
           </a>
         </p>
         <address class="event__address">
-          {{  event.location.address }}
+          {{ event.location.address }}
         </address>
       </footer>
     </section>
@@ -52,12 +59,20 @@
     agenda: Event[],
   }
 
-  type Venue = {
-    name: string,
-    events: Event[],
+  type CollapsibleEvent = Event & { expanded?: boolean }
+
+  type Ordering = (a: Event, b: Event) => number
+
+  enum Venues {
+    DOORNROOSJE = 'doornroosje',
   }
 
-  type EventItem = Event & { expanded?: boolean }
+  enum Orderings {
+    DATE_ASC = 'date ascending',
+    DATE_DESC = 'date descending',
+    PRICE_ASC = 'price ascending',
+    PRICE_DESC = 'price descending',
+  }
 
   @Component
   export default class Agenda extends Vue {
@@ -68,31 +83,76 @@
       }),
     }
 
-    private readonly venues = [
-      'doornroosje',
-    ]
+    private readonly venues: Venues[] = Object.values(Venues)
 
-    private events: EventItem[] = []
-
-    async mounted() {
-      this.events = (await Promise.all(
-        this.venues.map(async name => {
-          const response = await fetch(`/.netlify/functions/get_venue?venue=${ name }`)
-
-          if (!response.ok) {
-            console.error(`Unable to fetch results for venue '${ name }'`)
-            return []
-          }
-
-          return (await response.json() as Response).agenda
-        })
-      ))
-        .flat()
-        .sort((a, b) => a.startDate.localeCompare(b.startDate))
+    private readonly orderings: { [key in Orderings]: Ordering } = {
+      [Orderings.DATE_ASC]: (a: Event, b: Event) => a.startDate.localeCompare(b.startDate),
+      [Orderings.DATE_DESC]: (a: Event, b: Event) => b.startDate.localeCompare(a.startDate),
+      [Orderings.PRICE_ASC]: (a: Event, b: Event) => a.offers.price.localeCompare(b.offers.price),
+      [Orderings.PRICE_DESC]: (a: Event, b: Event) => b.offers.price.localeCompare(a.offers.price),
     }
 
-    toggle(event: EventItem): void {
-      this.events = this.events.map(item => item === event ? { ...item, expanded: !item.expanded } : item)
+    private eventsByVenue: { [venue in Venues]?: CollapsibleEvent[] } = {}
+
+    private loading: number = 0
+    private errors: string[] = []
+    private activeOrdering: Orderings = Orderings.DATE_ASC
+
+    mounted() {
+      this.fetchData()
+    }
+
+    async fetchData() {
+      try {
+        this.loading = this.venues.length
+        this.errors = []
+
+        await Promise.all(this.venues.map(async venue => {
+          try {
+            const response = await fetch(`/.netlify/functions/get_venue?venue=${ venue }`)
+
+            if (!response.ok) {
+              this.errors.push(`Kon geen resultaten ophalen voor ${ venue }`)
+              return
+            }
+
+            const agenda = (await response.json() as Response).agenda
+
+            this.eventsByVenue = {
+              ...this.eventsByVenue,
+              [venue]: agenda,
+            }
+          } finally {
+            this.loading -= 1
+          }
+        }))
+      } catch (err) {
+        console.error(err)
+        this.errors.push(err.message)
+      }
+    }
+
+    get events(): CollapsibleEvent[] {
+      return Object.values(this.eventsByVenue)
+        .flat()
+        .sort(this.orderings[this.activeOrdering])
+    }
+
+    toggle(event: CollapsibleEvent): void {
+      const copy = { ...this.eventsByVenue }
+
+      for (const venue of Object.values(Venues)) {
+        const events = copy[venue]
+        if (events) {
+          for (const item of events) {
+            if (item === event) {
+              copy[venue] = events.map(item => item === event ? { ...item, expanded: !item.expanded } : item)
+              this.eventsByVenue = copy
+              return
+            }
+          }
+        }
+      }
     }
 
     paragraphs(text: string): string[] {
