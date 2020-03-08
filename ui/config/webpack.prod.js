d3564857e74849d5f8ec4b5d554ac80ca8790084
Ruud van der Weide <rj@rdcl.dev>
2020-03-08 13:55:40 +0100

    Fixed copying of static files on build

--- ui/config/webpack.prod.js
+++ ui/config/webpack.prod.js
@@ -1,8 +1,16 @@
 'use strict'
 
-const { base } = require('./webpack.base')
+const CopyPlugin = require('copy-webpack-plugin')
+const { paths, base } = require('./webpack.base')
 
 module.exports = {
   ...base,
   mode: 'production',
+  plugins: [
+    new CopyPlugin([
+      { from: paths.PUBLIC, to: paths.DIST, ignore: ['index.html'] },
+    ]),
+
+    ...base.plugins,
+  ],
 }
