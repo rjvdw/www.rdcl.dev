9a7e1829c87e9bba1080b918808a020d839ab1f5
Ruud van der Weide <rj@rdcl.dev>
2020-04-14 19:53:45 +0200

    Added autocapitalize to HTML elements tool

--- ui/src/components/tools/Html.jsx
+++ ui/src/components/tools/Html.jsx
@@ -3,17 +3,24 @@ import React from 'react'
 const INPUT_ATTRS = {
   inputmode: ['none', 'text', 'tel', 'url', 'email', 'numeric', 'decimal', 'search'],
   enterkeyhint: ['enter', 'done', 'go', 'next', 'previous', 'search', 'send'],
+  autocapitalize: ['off', 'none', 'on', 'sentences', 'words', 'characters'],
 }
 
 export const Html = () => <>
   <h1>HTML Elements</h1>
 
+  <h2>Input Attributes</h2>
   <rdcl-input-grid>
-    { Object.entries(INPUT_ATTRS).flatMap(([attr, vals]) => vals.map(val =>
-      <React.Fragment key={ `${ attr }-${ val }` }>
-        <label htmlFor={ `${ attr }-${ val }` }><code>{ attr }="{ val }"</code></label>
-        <input id={ `${ attr }-${ val }` } type="text" { ...{ [attr]: val } }/>
+    { Object.entries(INPUT_ATTRS).flatMap(([attr, vals]) =>
+      <React.Fragment key={ attr }>
+        <h3 data-span={ 2 }>{ attr }</h3>
+        { vals.map(val =>
+          <React.Fragment key={ val }>
+            <label htmlFor={ `${ attr }-${ val }` }><code>{ val }</code></label>
+            <input id={ `${ attr }-${ val }` } type="text" { ...{ [attr]: val } }/>
+          </React.Fragment>
+        ) }
       </React.Fragment>
-    )) }
+    ) }
   </rdcl-input-grid>
 </>
