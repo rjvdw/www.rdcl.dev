e933be49735fa60b5348046e247934c2541bf47a
Ruud van der Weide <rj@rdcl.dev>
2021-08-29 11:32:36 +0200

    Added JSDoc to util.js

--- ui/src/components/util.js
+++ ui/src/components/util.js
@@ -1,6 +1,18 @@
 import { useEffect, useRef, useState } from 'react'
 import { history } from '../history'
 
+/**
+ * @typedef {(T) => void} Consumer<T>
+ */
+
+/**
+ * Returns a stateful value, and a function to update it, using the history state.
+ *
+ * @template T
+ * @param {string} key
+ * @param {T} initialState
+ * @return {[T, Consumer<T>]}
+ */
 export function useHistoryState(key, initialState) {
   const historyState = history.location.state || {}
   const [state, setState] = useState(historyState[key] || initialState)
@@ -13,6 +25,13 @@ export function useHistoryState(key, initialState) {
   ]
 }
 
+/**
+ * Returns a ref with autofocus.
+ *
+ * @template {HTMLOrSVGElement} T
+ * @param {T} initialValue
+ * @return {React.MutableRefObject<T>}
+ */
 export function useAutoFocusRef(initialValue = null) {
   const field = useRef(initialValue)
   useEffect(() => {
