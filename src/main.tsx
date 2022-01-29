14d8142883222237ec9548b8a58a48bc192b5661
Ruud van der Weide <rj@rdcl.dev>
2022-01-29 16:15:19 +0100

    Connected history to redux state

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { App } from './components/App'
 import { store } from './store'
+import { history } from './history'
+import { App } from './components/App'
 import './elements'
 import './styles/main.sass'
 import { updateScreenType } from './modules/screen'
+import { navigate } from './modules/routes'
 
 ReactDOM.render(
   <Provider store={ store }>
@@ -16,4 +18,28 @@ ReactDOM.render(
 
 window.addEventListener('resize', () => {
   store.dispatch(updateScreenType())
+}, {
+  passive: true,
+})
+
+document.body.addEventListener('click', event => {
+  if (event.defaultPrevented) {
+    return
+  }
+
+  if (event.ctrlKey || event.shiftKey || event.metaKey || event.button === 1) {
+    // user is trying to open a new tab, don't interfere
+    return
+  }
+
+  const element = event.composedPath().find(el => (el as HTMLAnchorElement).href) as HTMLAnchorElement
+
+  if (element && !element.dataset.noHistory && element.origin === window.location.origin) {
+    event.preventDefault()
+    history.push(element.pathname)
+  }
+})
+
+history.listen(({ location }) => {
+  store.dispatch(navigate(location))
 })
