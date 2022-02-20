a8adad13498c3b0e7398004b75a402ae236fa741
Ruud van der Weide <rj@rdcl.dev>
2022-02-20 10:24:20 +0100

    Added CI pipeline

---
+++ config/webpack.ci.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { paths, base } = require('./webpack.base')
+
+module.exports = {
+  ...base,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8888,
+    historyApiFallback: true,
+    static: {
+      directory: paths.PUBLIC,
+    },
+    client: {
+      overlay: {
+        warnings: true,
+        errors: true,
+      },
+    },
+    proxy: {
+      '/api': {
+        // TODO
+      },
+    },
+  },
+}
