ecde540dae46ee47cc911ce95ad48d70ebb6c0c1
Ruud van der Weide <rj@rdcl.dev>
2023-01-27 10:39:31 +0100

    Add search fields to Health

---
+++ src/pages/Health/FromAndTo.tsx
@@ -0,0 +1,58 @@
+import React, { FunctionComponent, useId } from 'react'
+import { useForm } from 'react-hook-form'
+
+type FromAndToProps = {
+  from?: string
+  to?: string
+  setSearch(value: SearchState): void
+}
+
+export type SearchState = {
+  from?: string
+  to?: string
+}
+
+export const FromAndTo: FunctionComponent<FromAndToProps> = ({
+  from,
+  to,
+  setSearch,
+}) => {
+  const id = useId()
+  const { register, handleSubmit, setValue } = useForm<SearchState>()
+
+  const clear = (key: keyof SearchState) => setValue(key, undefined)
+
+  return (
+    <form onSubmit={handleSubmit(setSearch)} className="health__search">
+      <rdcl-input-grid suffix>
+        <label data-start={1} htmlFor={`${id}:from`}>
+          From
+        </label>
+        <input
+          id={`${id}:from`}
+          type="date"
+          defaultValue={from}
+          {...register('from')}
+        />
+        <button type="button" onClick={() => clear('from')}>
+          Clear
+        </button>
+
+        <label data-start={1} htmlFor={`${id}:to`}>
+          To
+        </label>
+        <input
+          id={`${id}:to`}
+          type="date"
+          defaultValue={to}
+          {...register('to')}
+        />
+        <button type="button" onClick={() => clear('to')}>
+          Clear
+        </button>
+
+        <button data-start={2}>Search</button>
+      </rdcl-input-grid>
+    </form>
+  )
+}
--- src/pages/Health/Health.tsx
+++ src/pages/Health/Health.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { ActiveRoute } from '../../components/ActiveRoute'
 import { RequireLogin } from '../../components/RequireLogin'
 import { Title } from '../../components/Title'
+import { useHistoryState } from '../../util/useHistoryState'
+import { FromAndTo, SearchState } from './FromAndTo'
 import { HealthRecords } from './HealthRecords'
 import { NewEntry } from './NewEntry'
 import { useHealthRecords } from './hooks'
 
 export const Health = () => {
-  const { records, loading, error, refresh } = useHealthRecords()
+  const [{ from, to }, setSearch] = useHistoryState<SearchState>({})
+  const { records, loading, error, refresh } = useHealthRecords(from, to)
 
   return (
     <>
@@ -19,6 +22,9 @@ export const Health = () => {
       <NewEntry onSave={refresh} />
 
       <h2>Records</h2>
+
+      <FromAndTo from={from} to={to} setSearch={setSearch} />
+
       <HealthRecords
         records={records}
         loading={loading}
