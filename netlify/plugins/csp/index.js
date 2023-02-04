17418179a24485c74f67b9356455b8ca7a125f81
Ruud van der Weide <rj@rdcl.dev>
2023-02-04 15:08:08 +0100

    Only return CSP headers in production

---
+++ netlify/plugins/csp/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+export const onPreBuild = ({ netlifyConfig }) => {
+  for (const header of netlifyConfig.headers) {
+    if (header.values['X_CSP']) {
+      if (header.values['Content-Security-Policy']) {
+        console.log(
+          '[CSP][%s] replace "%s" with "%s"',
+          header.for,
+          header.values['Content-Security-Policy'],
+          header.values['X_CSP']
+        )
+      } else {
+        console.log('[CSP][%s] add "%s"', header.for, header.values['X_CSP'])
+      }
+      header.values['Content-Security-Policy'] = header.values['X_CSP']
+      delete header.values['X_CSP']
+    }
+  }
+}
