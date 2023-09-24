56965b96778f4b572b6f7c58d9225895f203dc0d
Ruud van der Weide <rj@rdcl.dev>
2023-09-24 09:39:45 +0200

    Base styling

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,4 +2,9 @@
   <title>rdcl.dev</title>
 </svelte:head>
 
-<h1>Hello, World!</h1>
+<h1>rdcl.dev</h1>
+
+<p>
+  Welcome to this page. I use this website to dump random stuff.
+  <a href="https://www.ruud.online">You can find my homepage here</a>.
+</p>
