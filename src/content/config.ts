a2cecfd1c6e0c04c6d8a221b948356d1368216cc
Ruud van der Weide <rj@rdcl.dev>
2024-11-08 21:40:30 +0100

    Move Blood on the Clocktower pages to separate project
    Blood on the Clocktower now lives at
    [rjvdw/blood-on-the-clocktower](https://github.com/rjvdw/blood-on-the-clocktower)

--- src/content/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCollection } from 'astro:content'
-import botcCharacterSchema from '$content/schema/botc-character'
-
-export const collections = {
-  'botc-characters': defineCollection({
-    type: 'content',
-    schema: botcCharacterSchema,
-  }),
-}
